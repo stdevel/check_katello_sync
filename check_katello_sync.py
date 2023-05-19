a66b1dafda559e5f11697263e7067ea0367a97dc
Christian Stankowic <info@cstan.io>
2023-05-19 21:42:07 +0200

    fix some linting

--- check_katello_sync.py
+++ check_katello_sync.py
@@ -10,14 +10,15 @@ within Foreman/Katello or Red Hat Satellite 6
 import argparse
 import logging
 import os
+import sys
 import stat
 import json
 import datetime
 import getpass
-from ForemanAPIClient import ForemanAPIClient
 from datetime import datetime
+from ForemanAPIClient import ForemanAPIClient
 
-__version__ = "0.5.4"
+__version__ = "0.6.0"
 """
 str: Program version
 """
@@ -54,45 +55,44 @@ int: total products
 
 def set_code(return_code):
     """
-    This function sets or updates the result code.
+    This function sets or updates the result code
 
     :param return_code: Return code
     :type return_code: int
     """
     global STATE
     if return_code > STATE:
-        #update return code
+        # update return code
         STATE = return_code
 
 
 
 def get_return_str():
     """
-    This function returns the result status based on the state code.
+    This function returns the result status based on the state code
     """
-    #get return string
+    # get return string
     if STATE == 3:
-        return "UNKNOWN"
+        _state = "UNKNOWN"
     elif STATE == 2:
-        return "CRITICAL"
+        _state = "CRITICAL"
     elif STATE == 1:
-        return "WARNING"
+        _state = "WARNING"
     else:
-        return "OK"
+        _state = "OK"
+    return _state
 
 
 
 def check_product(product):
     """
-    Checks a product for currency.
+    Checks a product for currency
 
     :param product: Product dictionary
     :type product: dict
     """
-    global PROD_OK, PROD_WARN, PROD_CRIT
-
-    #check if product unsynced
-    if product["last_sync"] == None:
+    # check if product unsynced
+    if product["last_sync"] is None:
         LOGGER.debug("Product '{0}' ({1}) is UNSYNCED!".format(
             product["label"], product["description"]
         ))
@@ -125,24 +125,24 @@ def check_product(product):
 
 def check_products():
     """
-    Checks products for currency.
+    Checks products for currency
     """
     global PROD_TOTAL
 
-    #get API result
+    # get API result
     result_obj = json.loads(
         FOREMAN_CLIENT.api_get(
             "/products?organization_id={}&per_page=1337".format(options.org)
         )
     )
 
-    #check for non-existing products
+    # check for non-existing products
     for product in options.include:
         if product not in [x["label"] for x in result_obj["results"]]:
             PROD_CRIT.append(product)
             set_code(2)
 
-    #check _all_ the products
+    # check _all_ the products
     for product in [x for x in result_obj["results"] if x["repository_count"] > 0]:
         PROD_TOTAL = PROD_TOTAL + 1
         if len(options.include) > 0:
@@ -154,9 +154,9 @@ def check_products():
         else:
             check_product(product)
 
-    #set output
+    # set output
 
-    #critical products
+    # critical products
     str_crit = ", ".join(PROD_CRIT)
     if len(PROD_CRIT) >= 1:
         str_crit = "Products non-existent or outdated more than {0} days: {1}".format(
@@ -166,7 +166,7 @@ def check_products():
     else:
         str_crit = ""
 
-    #warning products
+    # warning products
     str_warn = ", ".join(PROD_WARN)
     if len(PROD_WARN) >= 1:
         str_warn = "Products outdated up to {0} days: {1}".format(
@@ -176,14 +176,14 @@ def check_products():
     else:
         str_warn = ""
 
-    #ok products
+    # ok products
     str_ok = ", ".join(PROD_OK)
     if len(PROD_OK) >= 1:
         str_ok = "Products synchronized: {0}".format(str_ok)
     else:
         str_ok = ""
 
-    #perfdata
+    # perfdata
     perfdata = "|"
     if options.show_perfdata:
         perfdata = "{0} 'prod_total'={1};;;; " \
@@ -194,19 +194,20 @@ def check_products():
                 len(PROD_CRIT), options.outdated_crit,
             )
 
-    #final string
+    # final string
     output = "{0}{1}{2} {3} ".format(
         str_crit, str_warn, str_ok, perfdata
     )
-    #print result and die in a fire
+    # print result and die in a fire
     print("{0}: {1}".format(get_return_str(), output))
-    exit(STATE)
+    sys.exit(STATE)
 
 
 
 def get_credentials(prefix, input_file=None):
     """
-    Retrieves credentials for a particular external system (e.g. Satellite).
+    Retrieves credentials for a particular external system (e.g. Satellite)
+
     :param prefix: prefix for the external system (used in variables/prompts)
     :type prefix: str
     :param input_file: name of the auth file (default: none)
@@ -215,13 +216,12 @@ def get_credentials(prefix, input_file=None):
     if input_file:
         LOGGER.debug("Using authfile")
         try:
-            #check filemode and read file
+            # check filemode and read file
             filemode = oct(stat.S_IMODE(os.lstat(input_file).st_mode))
-            #filemode is e.g. "0o644" right now. Remove 'o' for comparison.
             filemode = filemode.replace("o", "")
-            if filemode == "0600" or filemode == "0400":
+            if filemode in ("0600", "0400"):
                 LOGGER.debug("File permission matches 0600 or 0400")
-                with open(input_file, "r") as auth_file:
+                with open(input_file, "r", encoding="utf-8") as auth_file:
                     s_username = auth_file.readline().replace("\n", "")
                     s_password = auth_file.readline().replace("\n", "")
                 return (s_username, s_password)
@@ -237,12 +237,12 @@ def get_credentials(prefix, input_file=None):
             return (s_username, s_password)
     elif prefix.upper()+"_LOGIN" in os.environ and \
         prefix.upper()+"_PASSWORD" in os.environ:
-        #shell variables
+        # shell variables
         LOGGER.debug("Checking {} shell variables".format(prefix))
         return (os.environ[prefix.upper()+"_LOGIN"], \
             os.environ[prefix.upper()+"_PASSWORD"])
     else:
-        #prompt user
+        # prompt user
         LOGGER.debug("Prompting for {} login credentials".format(prefix))
         s_username = input(prefix + " Username: ")
         s_password = getpass.getpass(prefix + " Password: ")
@@ -251,7 +251,9 @@ def get_credentials(prefix, input_file=None):
 
 
 def parse_options(args=None):
-    """Parses options and arguments."""
+    """
+    Parses options and arguments
+    """
 
     desc = '''check_katello_sync.py is used to check product synchronization
     within Foreman/Katello or Red Hat Satellite 6.x.
@@ -269,71 +271,73 @@ def parse_options(args=None):
     parser = argparse.ArgumentParser(description=desc, \
     epilog=epilog)
 
-    #move version here to fit current python 3 argparse
+    # move version here to fit current python 3 argparse
     parser.add_argument('--version', action='version', version=__version__)
 
-    #define option groups
+    # define option groups
     gen_opts = parser.add_argument_group("generic arguments")
     fman_opts = parser.add_argument_group("Foreman arguments")
     prod_opts = parser.add_argument_group("product arguments")
     filter_opts = parser.add_argument_group("product filter arguments")
     filter_opts_excl = filter_opts.add_mutually_exclusive_group()
 
-    #GENERIC ARGUMENTS
-    #-d / --debug
+    # GENERIC ARGUMENTS
+    # -d / --debug
     gen_opts.add_argument("-d", "--debug", dest="debug", default=False, \
     action="store_true", help="enable debugging outputs")
-    #-P / --show-perfdata
+    # -P / --show-perfdata
     gen_opts.add_argument("-P", "--show-perfdata", dest="show_perfdata", \
     default=False, action="store_true", \
     help="enables performance data (default: no)")
 
-    #FOREMAN ARGUMENTS
-    #-a / --authfile
+    # FOREMAN ARGUMENTS
+    # -a / --authfile
     fman_opts.add_argument("-a", "--authfile", dest="authfile", metavar="FILE",\
     default="", help="defines an auth file to use instead of shell variables")
-    #-s / --server
+    # -s / --server
     fman_opts.add_argument("-s", "--server", dest="server", metavar="SERVER", \
     default="localhost", help="defines the server to use (default: localhost)")
-    #--insecure
+    # --insecure
     fman_opts.add_argument("--insecure", dest="ssl_verify", default=True, \
     action="store_false", help="Disables SSL verification (default: no)")
-    #-o / --organization
+    # -o / --organization
     fman_opts.add_argument("-o", "--organization", dest="org", \
     action="store", default="", metavar="NAME|ID", help="specifies the " \
     "organization to check", required=True)
 
-    #PRODUCT ARGUMENTS
+    # PRODUCT ARGUMENTS
     prod_opts.add_argument("-w", "--outdated-warning", dest="outdated_warn", \
     default=2, metavar="DAYS", type=int, help="defines outdated products" \
     " warning threshold in days (default: 2)")
-    #-U / --outdated-critical
+    # -U / --outdated-critical
     prod_opts.add_argument("-c", "--outdated-critical", dest="outdated_crit", \
     default=5, metavar="DAYS", type=int, help="defines outdated products" \
     " critical threshold in days (default: 5)")
 
-    #PRODUCT FILTER ARGUMENTS
-    #-o / --organization
+    # PRODUCT FILTER ARGUMENTS
+    # -o / --organization
     filter_opts_excl.add_argument("-i", "--include", action="append", \
     default=[], type=str, dest="include", metavar="NAME", help="specifies " \
     " particular products to check (default: no)")
-    #-e / --exclude
+    # -e / --exclude
     filter_opts_excl.add_argument("-e", "--exclude", action="append", \
     default=[], type=str, dest="exclude", metavar="NAME", help="specfies " \
     " particular products to ignore (default: no)")
 
 
-    #parse options and arguments
+    # parse options and arguments
     options = parser.parse_args()
     return (options, args)
 
 
 
 def main(options):
-    """Main function, starts the logic based on parameters."""
+    """
+    Main function, starts the logic based on parameters
+    """
     global FOREMAN_CLIENT
 
-    #splitting is fun
+    # splitting is fun
     if len(options.include) == 1:
         options.include = options.include[0].split(',')
     if len(options.exclude) == 1:
@@ -342,18 +346,18 @@ def main(options):
     LOGGER.debug("Options: {0}".format(options))
     LOGGER.debug("Arguments: {0}".format(args))
 
-    #define client
+    # define client
     (fman_user, fman_pass) = get_credentials("Satellite", options.authfile)
     FOREMAN_CLIENT = ForemanAPIClient(
         options.server, fman_user, fman_pass, options.ssl_verify, "/katello"
     )
 
-    #get organization ID if string supplied
-    if options.org.isdigit() == False:
+    # get organization ID if string supplied
+    if options.org.isdigit() is False:
         options.org = FOREMAN_CLIENT.get_id_by_name(
             options.org, "organization")
 
-    #do the magic
+    # do the magic
     check_products()
 
 
@@ -361,12 +365,12 @@ def main(options):
 if __name__ == "__main__":
     (options, args) = parse_options()
 
-    #set logging level
+    # set logging level
     logging.basicConfig()
     if options.debug:
         LOGGER.setLevel(logging.DEBUG)
     else:
         LOGGER.setLevel(logging.ERROR)
 
-    #yes this is main function
+    # yes this is main function
     main(options)
